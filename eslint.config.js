687ceff71e24ee7499a1b80053742b90645d1052
Scott Lusk <scottluskcis@github.com>
2025-03-05 16:08:06 -0500

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import typescript from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescript,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      // Add any custom rules here
+    }
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  prettier
+];
