810d4e07ee9b8328a1faa09aff0273eb883c0d73
Scott Lusk <scottluskcis@github.com>
2025-03-06 10:11:48 -0500

    add support to pass arugments to main

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
+import { config } from 'dotenv';
 import { run } from './main';
 
-run().catch((error) => {
-    console.error('An error occurred:', error);
-    process.exit(1);
+// Load environment variables from .env file
+config();
+
+run({
+  accessToken: process.env.ACCESS_TOKEN,
+  baseUrl: process.env.BASE_URL || 'https://api.github.com',
+  disableTelemetry: process.env.DISABLE_TELEMETRY === 'true',
+  outputPath: process.env.OUTPUT_PATH,
+  owner: process.env.OWNER || '',
+  ownerType: process.env.OWNER_TYPE as any,
+  proxyUrl: process.env.PROXY_URL,
+  skipArchived: process.env.SKIP_ARCHIVED === 'true',
+  skipUpdateCheck: process.env.SKIP_UPDATE_CHECK === 'true',
+  verbose: process.env.VERBOSE === 'true',
+  appId: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  appInstallationId: process.env.APP_INSTALLATION_ID,
+}).catch((error) => {
+  console.error('An error occurred:', error);
+  process.exit(1);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
-export async function run() {
-    console.log('Hello, TypeScript!');
+interface Arguments {
+  accessToken?: string;
+  baseUrl: string;
+  disableTelemetry: boolean;
+  outputPath: string | undefined;
+  owner: string;
+  ownerType: OwnerType;
+  proxyUrl: string | undefined;
+  skipArchived: boolean;
+  skipUpdateCheck: boolean;
+  verbose: boolean;
+  appId?: string | undefined;
+  privateKey?: string | undefined;
+  appInstallationId?: string | undefined;
+}
+
+enum OwnerType {
+  Organization = 'organization',
+  User = 'user',
+}
+
+export async function run(opts: Arguments): Promise<void> {
+  console.log('Hello, TypeScript!');
 }
